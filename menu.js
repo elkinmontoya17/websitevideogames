3fb3e94da259a9aef85e81807bb781dfa09ff898
Elkin Montoya <elkinmontoya.development@gmail.com>
2021-10-25 21:12:47 -0500

    retoques finales entrega Proyecto final

--- menu.js
+++
@@ -1,9 +0,0 @@
-addEventListener('DOMContentLoaded', () => {
-    const botonMenu = document.querySelector('.botonMenu')
-    if (botonMenu) {
-        botonMenu.addEventListener('click', () => {
-            const menuItems = document.querySelector('.menuItems')
-            menuItems.classList.toggle('show')
-        })
-    }
-})
